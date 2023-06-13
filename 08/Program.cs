a3cb1b8e1739cb437d70fc183aa95b185336ee02
Marina <PyatkovaM@yandex.ru>
2023-06-13 18:01:47 +0300

    добавили решение 8 задачи

---
+++ 08/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+Console.Write("Введите целое число:  ");
+string userInput1 = Console.ReadLine() ?? "";
+int N = int.Parse(userInput1);
+
+int i = 0; 
+
+while (i < N)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine ($" {i} ");
+    }
+    i++;
+}
