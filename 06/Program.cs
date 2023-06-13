6cbf581f9541a106530587afc00bedaafcbcf089
Marina <PyatkovaM@yandex.ru>
2023-06-13 17:57:39 +0300

    добавили решение 6 задачи

---
+++ 06/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+Console.Write("Введите целое число:  ");
+string userInput1 = Console.ReadLine() ?? "";
+int a = int.Parse(userInput1);
+
+
+if (a % 2 == 1)
+{
+    Console.WriteLine ($" {a}  нечетное число ");
+}
+else
+{
+    Console.WriteLine ($" {a} четное число ");
+}
