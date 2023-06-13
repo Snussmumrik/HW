77345bcbbdae60f3e4b39fcdcfdb1b985f078c61
Marina <PyatkovaM@yandex.ru>
2023-06-13 17:54:12 +0300

    добавили решение 4 задачи

---
+++ 04/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+Console.Write("Введите первое целое число:  ");
+string userInput1 = Console.ReadLine() ?? "";
+int a = int.Parse(userInput1);
+
+Console.Write("Введите второе целое число: ");
+string userInput2 = Console.ReadLine() ?? "";
+int b = int.Parse(userInput2);
+
+Console.Write("Введите третье целое число: ");
+string userInput3 = Console.ReadLine() ?? "";
+int c = int.Parse(userInput3);
+
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.WriteLine ($" максимальное число --> {max} ");
